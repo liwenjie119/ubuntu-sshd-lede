1c44658957fd7f558a6bfe09c77117e8f19e8482
liwenjie119 <liwenjie119@126.com>
2021-01-06 23:09:34 +0800

    upload

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM       ubuntu:18.04
+MAINTAINER liwenjie119 "https://github.com/liwenjie119/ubuntu-sshd-lede"
+
+RUN curl https://raw.githubusercontent.com/liwenjie119/ubuntu-sshd-lede/master/build.sh|bash
+
+EXPOSE 22
+
+WORKDIR /home/123
+
+ENTRYPOINT  ["/usr/sbin/sshd", "-D"]
---
+++ build.sh
@@ -0,0 +1,23 @@
+apt-get update&&apt upgrade -y
+#sshd
+sed -ri 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+apt-get install -yqq sudo ssh net-tools vim language-pack-zh-hans bash-completion ttf-wqy-microhei
+mkdir /var/run/sshd
+echo 'export LC_ALL=zh_CN.UTF-8' >> /etc/profile
+echo 'root:root' |chpasswd
+useradd --create-home --no-log-init --shell /bin/bash 123
+adduser 123 sudo
+echo '123:123' | chpasswd
+echo '123 ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo 'export DISPLAY=:0' >> /etc/profile
+sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
+mkdir /root/.ssh
+mkdir /123/.ssh
+
+#lede
+apt-get -qq install $(curl -fsSL git.io/depends-ubuntu-1804)
+
+#clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+rm -rf /usr/share/dotnet /usr/local/lib/android /opt/ghc
