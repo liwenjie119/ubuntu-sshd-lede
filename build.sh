1904c7741230c396463a8bb88dd4c5a074813fa8
liwenjie119 <liwenjie119@126.com>
2022-02-21 16:10:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@ apt-get install -yqq sudo ssh net-tools vim screen language-pack-zh-hans bash-co
 mkdir /var/run/sshd
 echo 'export LC_ALL=zh_CN.UTF-8' >> /etc/profile
 echo 'root:root' |chpasswd
-useradd --create-home --no-log-init --shell /bin/bash 123
-adduser 123 sudo
-echo '123:123' | chpasswd
-echo '123 ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+useradd --create-home --no-log-init --shell /bin/bash user
+adduser user sudo
+echo 'user:user' | chpasswd
+echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 echo 'export DISPLAY=:0' >> /etc/profile
 sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
 mkdir /root/.ssh
-mkdir /home/123/.ssh
+mkdir /home/user/.ssh
 
 #lede
-apt-get -qq install $(curl -fsSL git.io/depends-ubuntu-2004)
+apt-get -qqy install $(curl -fsSL git.io/depends-ubuntu-2004)
 
 #clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
