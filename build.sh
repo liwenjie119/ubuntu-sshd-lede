fe1031af62ad3edfc571fec2265f4099d0c9f030
liwenjie119 <liwenjie119@126.com>
2022-02-21 17:09:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,10 +15,7 @@ mkdir /root/.ssh
 mkdir /home/user/.ssh
 
 #lede
-#apt-get -qqy install $(curl -fsSL git.io/depends-ubuntu-2004)
-apt install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev \
-libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo \
-libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+apt-get -qqy install $(curl -fsSL git.io/depends-ubuntu-2004)
 
 #clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
